34b5e90606a78755374a756a83adf470dce99b8d
Björn Dahlgren <bjorn@dahlgren.at>
2016-03-13 13:08:10 +0100

    Declare correct environment for production build

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -13,6 +13,11 @@ module.exports = {
       inject: false,
       template: 'index.html',
     }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": JSON.stringify("production")
+      }
+    }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
       minimize: true,
