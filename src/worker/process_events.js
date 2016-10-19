c43f339b3a02d86d27d98974f1396ea1a891a6b4
Björn Dahlgren <bjorn@dahlgren.at>
2016-10-19 02:09:44 +0200

    Refactored worker loading. Don't load events if world is not configured.

---
+++ src/worker/process_events.js
@@ -0,0 +1,51 @@
+import { projectileColor } from './colors';
+import { unitClassName, vehicleClassName } from './marker_classes';
+import { markerSize } from './marker_sizes';
+
+module.exports = function (events, world) {
+  var processedEvents = [];
+
+  events.forEach(function (event) {
+    if (event.projectile) {
+      processedEvents.push({
+        id: event.projectile.id,
+        x: event.projectile.position.x,
+        y: (world.size[1] - event.projectile.position.y),
+        color: projectileColor(event.projectile.side),
+        weight: 1,
+        timestamp: new Date(event.timestamp),
+        type: 'projectiles',
+      });
+    }
+
+    if (event.unit) {
+      processedEvents.push({
+        id: event.unit.id,
+        rotation: event.unit.position.dir,
+        x: event.unit.position.x,
+        y: (world.size[1] - event.unit.position.y),
+        className: unitClassName(event.unit),
+        markerSize: [16, 16],
+        name: event.unit.name,
+        timestamp: new Date(event.timestamp),
+        type: 'units',
+      });
+    }
+
+    if (event.vehicle && event.vehicle.name != "Ground" && event.vehicle.simulation != "thingX") {
+      processedEvents.push({
+        id: event.vehicle.id,
+        rotation: event.vehicle.position.dir,
+        x: event.vehicle.position.x,
+        y: (world.size[1] - event.vehicle.position.y),
+        className: vehicleClassName(event.vehicle),
+        markerSize: markerSize(event.vehicle.simulation),
+        name: event.vehicle.name,
+        timestamp: new Date(event.timestamp),
+        type: 'vehicles',
+      });
+    }
+  });
+
+  return processedEvents;
+};
