5f82d72c1cc4776f59afc728ffefa0fe3f74a21b
Björn Dahlgren <bjorn@dahlgren.at>
2017-01-08 03:08:38 +0100

    Play/Pause playback and change speed of events

--- src/components/EventsTicker.jsx
+++ src/components/EventsTicker.jsx
@@ -1,21 +1,54 @@
-import { Component } from 'react';
+import { Component, PropTypes } from 'react';
 import { tickEvents } from '../actions/events';
 
 export default class EventsTicker extends Component {
 
-  componentDidMount() {
-    this.timer = setInterval(this.tick, 100);
+  componentWillMount() {
+    if (this.props.isPlaying) {
+      this.timer = setInterval(this.tick.bind(this), 1000 / this.props.speed);
+    }
   }
 
   componentWillUnmount() {
-    clearInterval(this.timer);
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.speed != this.props.speed) {
+      if (this.timer) {
+        clearInterval(this.timer);
+        this.timer = setInterval(this.tick.bind(this), 1000 / nextProps.speed);
+      }
+    }
+
+    if (nextProps.isPlaying != this.props.isPlaying) {
+      if (nextProps.isPlaying) {
+        if (!this.timer) {
+          this.timer = setInterval(this.tick.bind(this), 1000 / nextProps.speed);
+        }
+      } else {
+        if (this.timer) {
+          clearInterval(this.timer);
+          this.timer = null;
+        }
+      }
+    }
   }
 
   tick() {
-    tickEvents(1000);
+    if (this.props.isPlaying) {
+      tickEvents(1000);
+    }
   }
 
   render() {
     return null;
   }
+}
+
+EventsTicker.propTypes = {
+  isPlaying: PropTypes.bool.isRequired,
+  speed: PropTypes.number.isRequired,
 }
