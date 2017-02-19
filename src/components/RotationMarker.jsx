5c0e8e39ff33719a5007947b0627d4befabb5bb9
Björn Dahlgren <bjorn@dahlgren.at>
2017-02-19 14:13:15 +0100

    Handle icon for RotationMarker more efficiently to avoid reloads

--- src/components/RotationMarker.jsx
+++ src/components/RotationMarker.jsx
@@ -1,17 +1,33 @@
+import { divIcon } from 'leaflet';
 import { Marker } from 'react-leaflet';
 
 export default class RotationMarker extends Marker {
   componentWillMount() {
+    this.props.icon = this.createIcon()
+
     super.componentWillMount();
     this.leafletElement.setRotationAngle(this.props.rotation);
     this.leafletElement.setRotationOrigin("center center");
   }
 
   componentDidUpdate(prevProps) {
+    if (this.props.className !== prevProps.className || this.props.markerSize[0] !== prevProps.markerSize[0] || this.props.markerSize[1] !== prevProps.markerSize[1]) {
+      this.props.icon = this.createIcon();
+    } else {
+      this.props.icon = prevProps.icon;
+    }
+
     super.componentDidUpdate(prevProps);
 
     if (this.props.rotation !== prevProps.rotation) {
       this.leafletElement.setRotationAngle(this.props.rotation);
     }
+  }
+
+  createIcon() {
+    return divIcon({
+      className: this.props.className,
+      iconSize: this.props.markerSize,
+    })
   }
 }
