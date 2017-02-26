e75427ec47d0b5c5c51fc6e46edeb7d032192460
Björn Dahlgren <bjorn@dahlgren.at>
2017-02-26 13:04:01 +0100

    Handle Rotation Marker icon creation properly instead of updating props

--- src/components/RotationMarker.jsx
+++ src/components/RotationMarker.jsx
@@ -3,23 +3,25 @@ import { Marker } from 'react-leaflet';
 
 export default class RotationMarker extends Marker {
   componentWillMount() {
-    this.props.icon = this.createIcon()
-
     super.componentWillMount();
+    
+    this.leafletElement.setIcon(this.createIcon());
     this.leafletElement.setRotationAngle(this.props.rotation);
     this.leafletElement.setRotationOrigin("center center");
   }
 
   componentDidUpdate(prevProps) {
-    if (this.props.className !== prevProps.className || this.props.markerSize[0] !== prevProps.markerSize[0] || this.props.markerSize[1] !== prevProps.markerSize[1]) {
-      this.props.icon = this.createIcon();
-    } else {
-      this.props.icon = prevProps.icon;
-    }
-
     super.componentDidUpdate(prevProps);
 
-    if (this.props.rotation !== prevProps.rotation) {
+    const classNameChanged = this.props.className !== prevProps.className;
+    const markerSizeChanged  = this.props.markerSize[0] !== prevProps.markerSize[0] || this.props.markerSize[1] !== prevProps.markerSize[1];
+    const rotationChanged = this.props.rotation !== prevProps.rotation;
+
+    if (classNameChanged || markerSizeChanged) {
+      this.leafletElement.setIcon(this.createIcon());
+    }
+
+    if (rotationChanged) {
       this.leafletElement.setRotationAngle(this.props.rotation);
     }
   }
