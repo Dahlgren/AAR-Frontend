9b3dfbe229253b50a9480a1351a5e73d0f1b97b4
Björn Dahlgren <bjorn@dahlgren.at>
2019-02-17 15:22:37 +0100

    Add support for overriding which AAR API to use

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 var express = require('express')
+var fs = require('fs')
 var favicon = require('serve-favicon')
 var path = require('path')
 var app = express()
 
 var staticPath = path.join(__dirname, 'build')
+var indexPath = path.join(staticPath, 'index.html')
+var indexFile = fs.readFileSync(indexPath, { encoding: 'utf-8' })
+
+var apiUrl = process.env.AAR_API_URL
+if (apiUrl) {
+  indexFile = indexFile.replace('AAR_API_URL = undefined', 'AAR_API_URL = \'' + apiUrl + '\'')
+}
 
 app.use(favicon(path.join(__dirname, 'favicon.ico')))
 app.use(express.static(staticPath))
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', {
-    root: staticPath
-  })
+  res.send(indexFile)
 })
 
 app.listen(process.env.PORT || 8080, function (err) {
